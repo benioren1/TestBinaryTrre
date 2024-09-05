54d978b033ed04a425f27419ec4bbb18f0dbc5cd
beni oren <benioren123beni@gmail.com>
2024-09-05 17:51:26 +0300

    finish finish finish

--- Program.cs
+++ Program.cs
@@ -15,27 +15,31 @@ public class Program
     public static  async Task Main(string[] args)
     {
        
+        //מטעין קובץ ראשון של הגנות
         Console.WriteLine();
         Console.WriteLine("loeading json to the list");
         Thread.Sleep(4000);
         List<Node> list= _servicToNode.LoeadJson();
         
-
+        //יוצר עץ של הגנות
         Console.WriteLine();
         Console.WriteLine("creating new tree");
         Thread.Sleep(4000);
         binaryTree =  _servicToNode.InsertToTree(list);
 
+        //מדפיס את כל העץ
         Console.WriteLine();
         Console.WriteLine("printing all the tree");
         Thread.Sleep(4000);
         binaryTree.PrintTree();
 
+        //מטעין קובץ התקפות
         Console.WriteLine();
         Console.WriteLine("loeading json to the list");
         Thread.Sleep(4000);
         List<ThreatsNode> listtereats = _servicToTereatsNode.LoeadJsonForTereats();
 
+        //מתחיל התקפות
         Console.WriteLine();
         Console.WriteLine("starting to attaking");
         Thread.Sleep(4000);
